2bd771388090df0b854b28cf7ff68e4725974699
Ekaterina Smolnikova <Ekaterina_Smolnikova@epam.com>
2018-08-31 09:16:36 +0400

    List<EventLogger> was added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,20 +2,23 @@ package com.example;
 
 import com.example.services.EventLogger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class App {
     @Autowired
     private Client client;
     @Autowired
-    @Qualifier("anotherConsoleEventLogger")
-    private EventLogger eventLogger;
+    //@Qualifier("anotherConsoleEventLogger")
+    private List<EventLogger> eventLoggers;
     @Autowired
     private Event event;
 
     public void logEvent(Event event) {
-        eventLogger.logEvent(event);
+        for (EventLogger eventLogger : eventLoggers) {
+            eventLogger.logEvent(event);
+        }
     }
 }
--- src/main/java/com/example/Config.java
+++ src/main/java/com/example/Config.java
@@ -1,12 +1,17 @@
 package com.example;
 
+import com.example.services.EventLogger;
+import com.example.services.impl.AnotherConsoleEventLogger;
+import com.example.services.impl.ConsoleEventLogger;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 @Configuration
@@ -23,5 +28,13 @@ public class Config {
     public Event event() {
         int id = new Random().nextInt(1000);
         return new Event(id, "Hello, I'm number ".concat(String.valueOf(id)), new Date());
+    }
+
+    @Bean
+    public List<EventLogger> eventLoggers(EventLogger consoleEventLogger, EventLogger anotherConsoleEventLogger) {
+        List<EventLogger> eventLoggers = new ArrayList<EventLogger>();
+        eventLoggers.add(consoleEventLogger);
+        eventLoggers.add(anotherConsoleEventLogger);
+        return eventLoggers;
     }
 }
